307fda32b0f4092ede01c72bfb466ee92e209c9f
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2025-05-28 00:02:19 +0200

    Directly asm syscall: bit smaller size

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 
 #![no_main]
-use std::io::{self, Write};
-use std::fs::File;
-use std::os::unix::io::FromRawFd;
+use core::arch::asm;
 
 
 const N: usize = 1024;
@@ -19,14 +17,41 @@ const fn create_yes_array() -> [u8; N] {
 }
 const YES_ARRAY: [u8; N] = create_yes_array();
 
-fn stdout() -> File {
-    unsafe { File::from_raw_fd(1) }
+// From https://github.com/jasonwhite/syscalls/blob/main/src/syscall/x86_64.rs
+// On x86-64, the following registers are used for args 1-6:
+// arg1: %rdi
+// arg2: %rsi
+// arg3: %rdx
+// arg4: %r10
+// arg5: %r8
+// arg6: %r9
+//
+// rax is used for both the syscall number and the syscall return value.
+
+#[inline]
+pub unsafe fn write(fd: usize, buffer: &[u8; N], count: usize) -> usize {
+    let mut ret: usize;
+    unsafe {
+        asm!(
+            "syscall",
+            inlateout("rax") 1usize => ret,
+            in("rdi") fd,
+            in("rsi") buffer.as_ptr(),
+            in("rdx") count,
+            out("rcx") _, // rcx is used to store old rip
+            out("r11") _, // r11 is used to store old rflags
+            options(nostack, preserves_flags)
+        );
+    }
+    ret
 }
+
 
 #[unsafe(no_mangle)]
 pub fn main(_argc: i32, _argv: *const *const u8) {
-    let mut stdout = stdout();
-    loop {
-        let _ = stdout.write(&YES_ARRAY);
+    unsafe {
+        loop {
+            let _ = write(1, &YES_ARRAY, N);
+        }
     }
 }
