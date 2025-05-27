7abfaf4a5375f6cd38f8a2569c98947f5e88376b
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2025-05-27 22:10:40 +0200

    Write buffers filled with y\n: ~6GBs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::io::{self, Write};
 fn main() -> io::Result<()>{
     let stdout = io::stdout();
     let mut handle = stdout.lock();
+    let buffer = b"y\n".repeat(1024/2);
     loop {
-        handle.write_all(b"y\n")?;
+        handle.write_all(buffer.as_slice())?;
     }
     Ok(())
 }
