eb236153ef48a1e4dbe93c59da21328872ee03ad
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2025-05-27 22:30:02 +0200

    Create a const array. No performance changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::io::{self, Write};
 
+const N: usize = 1024;
+const YES: [u8; 2] = *b"y\n";
+const fn create_yes_array() -> [u8; N] {
+    let mut res = [0; N];
+    let mut i = 0;
+    while i < res.len() {
+        res[i] = YES[0];
+        res[i + 1] = YES[1];
+        i += 2;
+    }
+    res
+}
+const YES_ARRAY: [u8; N] = create_yes_array();
+
+
 fn main() -> io::Result<()>{
     let stdout = io::stdout();
     let mut handle = stdout.lock();
-    let buffer = b"y\n".repeat(1024/2);
     loop {
-        handle.write_all(buffer.as_slice())?;
+        handle.write_all(&YES_ARRAY)?;
     }
-    Ok(())
 }
