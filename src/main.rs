5d8b4d868132cc6b963d2ae9ef34ece1b15fef85
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2025-05-27 23:13:03 +0200

    Use no_main: Down to 5kb

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+
+#![no_main]
 use std::io::{self, Write};
+use std::fs::File;
+use std::os::unix::io::FromRawFd;
+
 
 const N: usize = 1024;
 const YES: [u8; 2] = *b"y\n";
@@ -14,11 +19,14 @@ const fn create_yes_array() -> [u8; N] {
 }
 const YES_ARRAY: [u8; N] = create_yes_array();
 
+fn stdout() -> File {
+    unsafe { File::from_raw_fd(1) }
+}
 
-fn main() {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
+#[unsafe(no_mangle)]
+pub fn main(_argc: i32, _argv: *const *const u8) {
+    let mut stdout = stdout();
     loop {
-        let _ = handle.write(&YES_ARRAY);
+        let _ = stdout.write(&YES_ARRAY);
     }
 }
