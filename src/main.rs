fe873f6b8331c1ac7ede39fcaed9217df9019612
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2025-06-03 23:41:04 +0200

    Use vmsplice with 1024*1024 buffers
    https://mazzo.li/posts/fast-pipes.htm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 
 #![no_main]
 #![no_std]
-use core::arch::asm;
+
+use libc::F_SETPIPE_SZ;
 extern crate libc;
 
-const N: usize = 1024;
+const N: usize = 1024*1024;
 const YES: [u8; 2] = *b"y\n";
 const fn create_yes_array() -> [u8; N] {
     let mut res = [0; N];
@@ -18,40 +19,17 @@ const fn create_yes_array() -> [u8; N] {
 }
 const YES_ARRAY: [u8; N] = create_yes_array();
 
-// From https://github.com/jasonwhite/syscalls/blob/main/src/syscall/x86_64.rs
-// On x86-64, the following registers are used for args 1-6:
-// arg1: %rdi
-// arg2: %rsi
-// arg3: %rdx
-// arg4: %r10
-// arg5: %r8
-// arg6: %r9
-//
-// rax is used for both the syscall number and the syscall return value.
-
-#[inline]
-pub unsafe fn write(fd: usize, buffer: &[u8; N], count: usize) -> usize {
-    let mut ret: usize;
-    unsafe {
-        asm!(
-            "syscall",
-            inlateout("rax") 1usize => ret,
-            in("rdi") fd,
-            in("rsi") buffer.as_ptr(),
-            in("rdx") count,
-            out("rcx") _, // rcx is used to store old rip
-            out("r11") _, // r11 is used to store old rflags
-            options(nostack, preserves_flags)
-        );
-    }
-    ret
-}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
     unsafe {
+        libc::fcntl(1, F_SETPIPE_SZ, N);
+        let iovec = libc::iovec {
+            iov_base: YES_ARRAY.as_mut_ptr() as *mut core::ffi::c_void,
+            iov_len: N,
+        };
         loop {
-            let _ = write(1, &YES_ARRAY, N);
+            libc::vmsplice(1, &iovec, 1, libc::SPLICE_F_GIFT & libc::SPLICE_F_NONBLOCK);
         }
     }
 }
